b15eeaa742c302426138814de5ac2ea406449589
ombharatiya <om.dkcarmel@gmail.com>
2018-01-14 06:28:33 +0530

    Added Find Max & Min Functions

--- bst1.cpp
+++ bst1.cpp
@@ -38,15 +38,43 @@ bool Search(BstNode* root, int data) {
 	else return Search(root->right, data);
 }
 
+int FindMin(BstNode* root) {
+	if(root==NULL) {
+		cout<<"NULL ROOT ERROR";
+		return -1;
+	}
+	else if (root->left == NULL) {
+		return root->data;
+	}
+	else
+		return FindMin(root->left);
+}
+
+int FindMax(BstNode* root) {
+	if(root==NULL) {
+		cout<<"NULL ROOT ERROR";
+		return -1;
+	}
+	else if (root->right == NULL) {
+		return root->data;
+	}
+	else
+		return FindMax(root->right);
+}
+
 int main() {
-	BstNode* root = NULL;
+	BstNode* root = NULL;int num;
 	root = Insert(root, 10); root = Insert(root, 20);
 	root = Insert(root, 30); root = Insert(root, 40);
 	root = Insert(root, 50); root = Insert(root, 60);
 	root = Insert(root, 70); root = Insert(root, 80);
 	root = Insert(root, 90); root = Insert(root, 100);
-	cout<< Search(root, 40) << "\n";
-	cout<< Search(root, 35);
+	// cout<< Search(root, 40) << "\n";
+	// cout<< Search(root, 35);
+	cout << "Enter the no. to Search :";
+	cin >> num;
+	cout << (Search(root, num)? "true\n": "false\n");
+	cout << "Min :" << FindMin(root) <<"\nMax :" << FindMax(root);
 	return 0;
 }
 
